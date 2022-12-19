b7b2d8a1376c3b19a2bd595078735fcf5d341154
Muhtasim Fuad <muhtasim@greentomatomedia.com>
2022-12-19 14:02:36 +0700

    settting up decorators

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import { useState } from 'react'
+import React from "react";
+import {
+  Editor,
+  EditorState,
+  CompositeDecorator,
+  DraftDecorator,
+} from "draft-js";
+import "draft-js/dist/Draft.css";
+import "./draft.css";
+
+function UltimateEditor() {
+  const handleStrategy: DraftDecorator["strategy"] = (
+    contetBlock,
+    callback,
+    contentState,
+  ) => {};
+
+  const handleComponent: DraftDecorator["component"] = () => {};
+
+  const compositeDecorator = new CompositeDecorator([
+    {
+      strategy: handleStrategy,
+      component: handleComponent,
+    },
+  ]);
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty(),
+  );
+
+  return <Editor editorState={editorState} onChange={setEditorState} />;
+}
 
 function App() {
-
   return (
     <div>
       <h2>Learning Draftjs</h2>
+      <div className="border-2 border-gray-200 h-64">
+        <UltimateEditor />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
